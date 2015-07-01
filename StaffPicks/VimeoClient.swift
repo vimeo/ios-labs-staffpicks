b518fcfba1a8a5916553a8905eb4e3d8ef3f4ed3
Rob Huebner <huebnerob@gmail.com>
2015-07-01 16:08:42 -0400

    session 3

--- StaffPicks/VimeoClient.swift
+++ StaffPicks/VimeoClient.swift
@@ -8,14 +8,63 @@
 
 typealias ServerResponseCallback = (videos: Array<Video>?, error: NSError?) -> Void
 
+let BaseURL = "https://api.vimeo.com/"
+let StaffPicksEndpoint = "channels/staffpicks/videos"
+let AccessToken = "ccc022b6c3c6dcd2025cb92d7294dc07"
+
 import Foundation
 
 class VimeoClient {
     
     static func staffpicks(callback: ServerResponseCallback) {
-        
-        callback(videos: [], error: nil)
-        
+
+        let staffpicksURL = NSURL(string: BaseURL + StaffPicksEndpoint)
+
+        if let constStaffPicksURL = staffpicksURL
+        {
+            var request = NSMutableURLRequest(URL: constStaffPicksURL)
+            request.addValue("Bearer " + AccessToken, forHTTPHeaderField: "Authorization")
+            request.HTTPMethod = "GET"
+            
+            var task = NSURLSession.sharedSession().dataTaskWithRequest(request, completionHandler: { (data: NSData!, response: NSURLResponse!, error: NSError!) -> Void in
+                
+                if error != nil
+                {
+                    println("request failed")
+                    
+                    callback(videos: nil, error: error)
+                    
+                    return
+                }
+                
+                let dataString = NSString(data: data, encoding: NSUTF8StringEncoding)
+                
+                println("data is \(dataString)")
+                
+                var JSONError: NSError?
+                let jsonDictionary = NSJSONSerialization.JSONObjectWithData(data, options: nil, error: &JSONError) as? [String:AnyObject]
+                
+                println("dictionary is \(jsonDictionary)")
+                
+                if let constJsonDictionary = jsonDictionary
+                {
+                    let videos = constJsonDictionary["data"] as? [[String:AnyObject]]
+                    
+                    if let constVideos = videos
+                    {
+                        for jsonVideo in constVideos
+                        {
+                            // TODO 
+                        }
+                    }
+                }
+                
+                
+                callback(videos: [], error: nil)
+            })
+            
+            task.resume()
+        }
     }
     
 }
